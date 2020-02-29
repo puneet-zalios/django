711ba240ced1aa80304da653bc23953da939d53c
Divick <divick@gorapid.io>
2020-02-29 15:28:46 +0530

    Some adjustements to request context processors.

--- fusion/settings.py
+++ fusion/settings.py
@@ -83,11 +83,11 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
-                'django.template.context_processors.static',
-                'django.template.context_processors.tz',
             ],
         },
     },
